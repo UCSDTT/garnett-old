f6a504c8a1cdf7903210b87296a9fdbd7a5efb77
Michael Luo <mchl.luosc@gmail.com>
2014-05-21 11:44:53 +0000

    fixed syntax error

--- initDB.js
+++ initDB.js
@@ -1,7 +1,7 @@
-var pg = require('pg').native
-  , conString = process.env.DATABASE_URL || "postgres://ttuser:ttuser@localhost:5432/" + local_database_name;
-  , client
-  , query;
+var pg = require('pg').native;
+var conString = process.env.DATABASE_URL || "postgres://ttuser:ttuser@localhost:5432/" + local_database_name;
+var client;
+var query;
 
 client = new pg.Client(conString);
 client.connect();
